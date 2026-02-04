1d2b9c053770bbfd7182116fc75c364d151feb23
Teagen.A.M <88509144+TeagenMcManus@users.noreply.github.com>
2026-02-04 19:16:46 +0000

    Used chat gbt to fix the code so that i can contine working on it with OOP horse race

--- HorseRace.cpp
+++ HorseRace.cpp
@@ -2,79 +2,67 @@
 #include <cstdlib>
 #include <ctime>
 
-//Variables
 const int amountHorse = 6;
 const int traLen = 10;
-void advance(int horseNum, int* horses);
+
+void advance(int* horses);
 void printLane(int horseNum, int* horses);
-bool isWinner(int horseNum, int* horses);
+bool isWinner(int* horses);
+void resetHorses(int* horses);
 
 int main() {
     srand(time(NULL));
-    //Variables
-    int horses[amountHorse] = {0, 0, 0, 0, 0};
 
-    //Prints the track for every horse number
-    for (int i = 1; i < amountHorse; i++) {
-        printLane(i, horses);
-    };
+    int horses[amountHorse] = {0};
 
-    //Kills the function
-    return 0;
-};
+    while (true) {
+        system("cls"); 
 
+        advance(horses);
 
-
-
-bool isWinner(int horseNum, int* horses) {
-    int userIn;
-    if (horses[horseNum] == traLen) {
-        std::cout << "Press Enter To Start A New Match: ";
-        std::cin >> userIn;
-        return 0;
-    }
-    else {
-        std::cout << "Press Enter To Continues: ";
-        std::cin >> userIn;
-        for (int i = 1; i < amountHorse; i++) {
+        for (int i = 0; i < amountHorse; i++) {
             printLane(i, horses);
-        };
-    }
-};
-
-void advance(int horseNum, int* horses) {
-    int coin = rand() % 2;
-    std::cout << coin << std::endl;
-    horses[horseNum] += coin;
-    //horses[horseNum - 1] = coin + horses[horseNum - 1];
-    //std::cout << "Horse #: " << horses[horseNum] << " Position: " << horses[horseNum - 1];
-   // std::cout <<std::endl;
-   if (horseNum == 5) { 
-    isWinner(horseNum, horses);
-   }
-   else {
-    std::cout<<"didntwork" << horseNum<< std::endl;
-   };
-};
-
-//Function to print out each lane
-void printLane(int horseNum, int* horses) {
-
-    //Loops to add in the lanes and the postiiton of the horses
-    for (int j = 0; j < traLen; j++) {
-
-        //Checks to see if the horse is in that position
-        if (j == horses[horseNum]){
-
-            //Prints the horses number
-            std::cout << horseNum;
         }
 
-        //If not then print out -
-        else {
+        if (isWinner(horses)) {
+            std::cout << "\nWinner! Press Enter to start a new race...";
+            std::cin.ignore();
+            std::cin.get();
+            resetHorses(horses);
+        }
+
+        std::cin.get(); // pause between turns
+    }
+}
+
+void advance(int* horses) {
+    for (int i = 0; i < amountHorse; i++) {
+        horses[i] += rand() % 2;
+        if (horses[i] > traLen)
+            horses[i] = traLen;
+    }
+}
+
+void printLane(int horseNum, int* horses) {
+    for (int j = 0; j < traLen; j++) {
+        if (j == horses[horseNum])
+            std::cout << horseNum;
+        else
             std::cout << "|";
-        };
-    };
+    }
     std::cout << std::endl;
-    advance(horseNum, horses);
-};
+}
+
+bool isWinner(int* horses) {
+    for (int i = 0; i < amountHorse; i++) {
+        if (horses[i] >= traLen)
+            return true;
+    }
+    return false;
+}
+
+void resetHorses(int* horses) {
+    for (int i = 0; i < amountHorse; i++) {
+        horses[i] = 0;
+    }
+}
